c27d9333a949bba4057cecb9ca328a51a0d6230f
刘馨怡 <43378097+LXYykdf@users.noreply.github.com>
2019-05-25 13:25:18 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,20 @@
-def randomapart(lines): 
+def pri(out):   # 将三维数组格式改为给定目标格式的函数 
+     
+    lines_new = [] 
+    for lines in out: 
+        blank3=[] 
+        for xj in lines: 
+            xja = list(map(str,xj)) 
+            a = ' '.join(xja) 
+            blank1 = [[]] 
+            blank1[0] = a 
+            blank3.append(blank1) 
+        string = '' 
+        for k in range(len(blank3)): 
+            if k == len(blank3)-1: 
+                string = string + blank3[k][0] + '    ' + '\n'  # 增加换行 
+            else: 
+                string = string + blank3[k][0] + '    ' 
+        lines_new.append(string) 
  
-    len1 = len(lines) 
-    part = [] 
-    randomnunm1 = random.randrange(len1) 
-    part.append(randomnunm1) 
-    len2 = len(lines[randomnunm1]) 
-    randomnunm2 = random.randrange(len2) 
-    part.append(randomnunm2) 
- 
-    return part
+    return lines_new
