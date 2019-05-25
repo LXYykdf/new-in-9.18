fe241788823e51a264aace33eac4317cb1d8bf02
刘馨怡 <43378097+LXYykdf@users.noreply.github.com>
2019-05-25 13:21:13 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,17 +1,9 @@
-def inversionOne(part, lines):  # 部分倒影 
+def extend(num, lines):  # 整体延长,生成新list 
  
     newlines = copy.deepcopy(lines) 
-    x = newlines[part[0]][part[1]][0] 
-    for i in range(len(newlines[part[0]][part[1]])): 
-        newlines[part[0]][part[1]][i] = 2 * x - newlines[part[0]][part[1]][i] 
- 
-    return newlines 
- 
-def inversionOne(part, lines):  # 部分倒影 
- 
-    newlines = copy.deepcopy(lines) 
-    x = newlines[part[0]][part[1]][0] 
-    for i in range(len(newlines[part[0]][part[1]])): 
-        newlines[part[0]][part[1]][i] = 2 * x - newlines[part[0]][part[1]][i] 
+    for i in range(len(newlines)): 
+        for j in range(len(newlines[i])): 
+            for k in range(len(newlines[i][j])): 
+                newlines[i][j][k] *= num 
  
     return newlines
